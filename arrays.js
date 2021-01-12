797dd76e7f8ddf0ea9a9b4dba06572201394c1ce
Aaron Lutas <alutas95@gmail.com>
2021-01-12 00:16:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand" , "kitkat", "skittles"];
 
-function addElementToBeginningOfArray() {
-  const addElementToBeginningOfArray = [];
-  addElementToBeginningOfArray.unshift
-}
 
-function destructivelyAddElementToBeginningOfArray() {
 
-}
+
